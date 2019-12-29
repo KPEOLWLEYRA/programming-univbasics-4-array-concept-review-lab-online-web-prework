af20a2991d2eb800126e748b553be86cd407caff
Kelly Powers <kpowers.nj@gmail.com>
2019-12-29 20:09:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while counter<array.length do
+    if array[counter] == value_to_find
+      p counter
+    end
+    counter +=1 
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  new_array = array.sort.reverse
+  p new_array[0]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  new_array = array.sort
+  p new_array[0]
 end
